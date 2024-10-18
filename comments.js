09cb5e467226418409b0b97f46e925849af04ce8
Omar Aedo <168685350+OmarAedoBeta@users.noreply.github.com>
2024-10-17 19:12:50 -0500

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,10 +1,10 @@
 // Create web server
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var comments = require('./comments.json');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+let comments = require('./comments.json');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -17,9 +17,39 @@ app.get('/api/comments', function(req, res) {
 
 // POST comments
 app.post('/api/comments', function(req, res) {
-  var comment = {
+  const comment = {
     id: Date.now(),
-    // additional properties
+    username: req.body.username,
+    message: req.body.message
   };
-  // additional logic
+  
+  comments.push(comment);
+  
+  fs.writeFile('./comments.json', JSON.stringify(comments, null, 2), (err) => {
+    if (err) {
+      res.status(500).send('Failed to save comment.');
+    } else {
+      res.status(201).json(comment);
+    }
+  });
+});
+
+// DELETE comment by ID
+app.delete('/api/comments/:id', function(req, res) {
+  const id = parseInt(req.params.id, 10);
+  comments = comments.filter(comment => comment.id !== id);
+  
+  fs.writeFile('./comments.json', JSON.stringify(comments, null, 2), (err) => {
+    if (err) {
+      res.status(500).send('Failed to delete comment.');
+    } else {
+      res.status(200).send('Comment deleted.');
+    }
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
